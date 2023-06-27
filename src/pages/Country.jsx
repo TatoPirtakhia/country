62427221ec1a6e5a499a28e1bccdb35c099a52de
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-06-27 15:24:13 +0400

    add info

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -26,6 +26,16 @@ function Country(props) {
 
     setShowNav(true);
   };
+
+  const curenci = (data) => {
+    const currencyNames = Object.keys(data).map((key) => {
+      const currency = data[key];
+      return currency.name;
+    });
+  
+    return currencyNames.join(", ");
+  };
+  
   return (
     <div className="pt-10 w-full flex flex-col items-center">
       <div className="flex flex-col items-start  w-[90%] gap-2 ">
@@ -38,11 +48,41 @@ function Country(props) {
           </div>
           <p>Back</p>
         </div>
-        <div>
+        <div className="mt-[65px]">
           {filteredCountry.map((data, index) => {
             return (
-              <div key={index}>
-                <img src={data.flags.png} alt="" />
+              <div key={index} className="flex flex-col items-start">
+                <img src={data.flags.png} alt="img" className="rounded-md" />
+                <h1 className="mt-10 nunito font-[800] text-[22px]">
+                  {data.name.common}
+                </h1>
+                <p>{`Native Name`}</p>
+                <p>
+                  Population: <span>{data.population.toLocaleString()}</span>
+                </p>
+                <p>
+                  Region: <span>{data.region}</span>{" "}
+                </p>
+                <p>
+                  Sub Region: <span>{data.subregion}</span>
+                </p>
+                <p>
+                  Capital:<span>{data.capital}</span>
+                </p>
+                <p>
+                  Top Level Domain: <span>{data.tld[0]}</span>
+                </p>
+                <p>
+                  Currencies: <span>{curenci(data.currencies)}</span>
+                </p>
+                <p></p>
+                <p></p>
+                <p></p>
+
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
               </div>
             );
           })}
